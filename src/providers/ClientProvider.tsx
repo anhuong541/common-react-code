75e4912633998919d6f24fea7dd4beb975257824
anhuong541 <nguyenxuananhuong541@gmail.com>
2026-01-27 17:02:01 +0700

    fix: remove suspence because of glitch when fetching

--- src/providers/ClientProvider.tsx
+++ src/providers/ClientProvider.tsx
@@ -1,18 +1,15 @@
 'use client'
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Suspense } from 'react'
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import ContextProvider from '@/contexts/ContextProvider'
 
 const queryClient = new QueryClient()
 
 export default function ClientProvider({ children }: { children: React.ReactNode }) {
   return (
-    <Suspense fallback={<div></div>}>
-      <ContextProvider>
-        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-      </ContextProvider>
-    </Suspense>
+    <ContextProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </ContextProvider>
   )
 }
